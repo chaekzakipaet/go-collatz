7f61230bdf727ef545887f4199f0080af6670b20
chaekzakipaet <forest-dweller@outlook.com>
2024-03-26 00:22:28 +0500

    Add the condition

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	n := 23
+	n := 42
 	fmt.Println(n)
-	n = n*3 + 1
-	fmt.Println(n)
-	n = n / 2
+
+	if n%2 == 1 {
+		n = n*3 + 1
+	} else {
+		n = n / 2
+	}
 	fmt.Println(n)
 }
