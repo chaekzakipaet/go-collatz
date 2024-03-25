4a3895394afc44c9f18e81492f483033d24959db
chaekzakipaet <forest-dweller@outlook.com>
2024-03-26 00:33:26 +0500

    Add the loop

--- main.go
+++ main.go
@@ -3,13 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	n := 42
+	n := 23
 	fmt.Println(n)
 
-	if n%2 == 1 {
-		n = n*3 + 1
-	} else {
-		n = n / 2
+	for n != 1 {
+		if n%2 == 1 {
+			n = n*3 + 1
+		} else {
+			n = n / 2
+		}
+
+		fmt.Println(n)
 	}
-	fmt.Println(n)
 }
